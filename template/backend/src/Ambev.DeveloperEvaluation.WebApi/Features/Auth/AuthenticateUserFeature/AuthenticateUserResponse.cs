bffcff3950b24a5393c33739f9d43e9e1100f7b3
filipefk <filipefk@gmail.com>
2025-02-25 21:28:40 -0300

    feat: Update de usuários #4 Normalizando o nome do usuário para UserName

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserResponse.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserResponse.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Ambev.DeveloperEvaluation.WebApi.Features.Auth.AuthenticateUserFeature;
 
 /// <summary>
@@ -15,12 +13,12 @@ public sealed class AuthenticateUserResponse
     /// <summary>
     /// Gets or sets the user's email address
     /// </summary>
-    public string Email { get; set; } = string.Empty;   
+    public string Email { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the user's full name
     /// </summary>
-    public string Name { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
 
     /// <summary>
     /// Gets or sets the user's role in the system
