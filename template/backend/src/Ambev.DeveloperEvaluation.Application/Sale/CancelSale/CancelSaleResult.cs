143f194dfb307797451ce76de2092cf2b79adbe5
filipefk <filipefk@gmail.com>
2025-03-06 14:58:14 -0300

    feat: Criar endpoint para cancelar venda #49

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sale/CancelSale/CancelSaleResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sale.CancelSale;
+
+/// <summary>
+/// Response model for CancelSale operation
+/// </summary>
+public class CancelSaleResult
+{
+    /// <summary>
+    /// Indicates whether the cancelation was successful
+    /// </summary>
+    public bool Success { get; set; }
+}
